fa8921bd9425c800c1f75bddb710ed1d440cfbd1
Matheus Reis <math.reis.dev@gmail.com>
2021-09-25 20:09:24 -0300

    showmenu

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,7 +2,7 @@ public class BankAccount {
 
 	public static void main(String[] args) {
 
-		Account matheus = new Account("Matheus Reis", "A00001");
+		Account matheus = new Account("Matheus Reis", "ID: A00001");
 		matheus.showMenu();
 	}
 
