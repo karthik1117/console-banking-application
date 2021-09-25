efd98398269884a2ad76ad9e8f0307f99aa88338
Matheus Reis <math.reis.dev@gmail.com>
2021-09-25 10:29:54 -0300

    Update FirstBank.java

--- src/FirstBank.java
+++ src/FirstBank.java
@@ -1,17 +1,11 @@
-// Visit compscicentral.com, the #1 resource for Comp Sci students
-
 public class FirstBank {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Account tim = new Account("Tim Statler", "A00001");
-		
-		
+				
 		Account beyonce = new Account("Beyonce", "B00002");
-		beyonce.showMenu();
-		
-		
+		beyonce.showMenu();	
 	}
 
 }
